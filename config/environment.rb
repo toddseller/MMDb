a4cfb381f40a5c4c4c9ba4190cfae6c6917392d5
Todd Seller <me@toddseller.com>
2016-09-10 10:26:00 -0700

    Reorganize css files for easier access

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,6 @@ require 'sinatra/json'
 
 require 'erb'
 require 'bcrypt'
-# require 'faker'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
