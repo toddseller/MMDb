7cb4b0d76387d914a64354a6213ce44990cc14fc
Todd Seller <me@toddseller.com>
2017-04-03 06:37:25 -0700

    Add HD to popup

--- public/js/mmdb.js
+++ public/js/mmdb.js
@@ -448,6 +448,7 @@ var activateModal = function (event) {
 }
 
 var displayMovieModal = function (response) {
+  console.log(response)
   $('#movie .modal-content').empty().append(response)
   $('#movie').modal('show')
 }
@@ -459,7 +460,6 @@ var editMovie = function (event) {
 }
 
 var displayEditForm = function (response) {
-  console.log(response)
   $('.modal-body').replaceWith(response)
   $('.modal-footer').hide()
 }
