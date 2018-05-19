a8709600df9811b4bc7411a44a7a258f55444500
Todd Seller <me@toddseller.com>
2018-05-19 09:58:49 -0700

    Fix issue with episodes not having plot and remove episodes from list that have not aired

--- app/models/show.rb
+++ app/models/show.rb
@@ -25,12 +25,12 @@ class Show < ActiveRecord::Base
 
     if series.count == 0
       token_response = tvdb_call("https://api.thetvdb.com/refresh_token")
-      if token_response[:code] == '200'
-        heroku_call(token_response[:body]['token'])
-      else
-        token_response = tvdb_auth()
-        heroku_call(token_response[:body]['token'])
-      end
+      # if token_response[:code] == '200'
+      #   heroku_call(token_response[:body]['token'])
+      # else
+      #   token_response = tvdb_auth()
+      #   heroku_call(token_response[:body]['token'])
+      # end
 
       first_response = tvdb_call("https://api.thetvdb.com/search/series?name=" + URI.encode(t))
 
@@ -62,7 +62,8 @@ class Show < ActiveRecord::Base
 
   def self.get_episodes(id, season)
     episodes = []
-
+    p id
+    p season
     if id.include? 'tvdb'
       id = id.gsub(/tvdb/,'')
       id = id[0...-season.to_s.length]
@@ -71,8 +72,9 @@ class Show < ActiveRecord::Base
 
       first_response[:body]['data'].each do |e|
         preview = "https://www.thetvdb.com/banners/episodes/" + id.to_s + "/" + e['id'].to_s + ".jpg"
-        episode = {title: e['episodeName'], date: convert_date(e['firstAired']), plot: get_plot(e['overview']), tv_episode: e['airedEpisodeNumber'], preview: preview}
-        episodes << episode
+        plot = e['overview'] ? get_plot(e['overview']) : ''
+        episode = {title: e['episodeName'], date: convert_date(e['firstAired']), plot: plot, tv_episode: e['airedEpisodeNumber'], preview: preview}
+        episodes << episode if Date.parse(e['firstAired']).past?
       end
     else
       episodes_response = JSON.parse(HTTParty.get('https://itunes.apple.com/lookup?id=' + id + '&country=us&entity=tvEpisode'))
