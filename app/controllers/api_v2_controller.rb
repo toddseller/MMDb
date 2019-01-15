9464243bc24482c59c0c5a09a8ff1dab6ef16d54
Todd Seller <todd.seller@optimizely.com>
2019-01-15 15:18:51 -0800

    Retry authentication

--- app/controllers/api_v2_controller.rb
+++ app/controllers/api_v2_controller.rb
@@ -14,9 +14,24 @@ namespace '/api/v2' do
   end
 
   get '/movies' do
-    options = { algorithm: 'HS256', iss: ENV['JWT_ISSUER'] }
-    bearer = env.fetch('HTTP_AUTHORIZATION', '').slice(7..-1)
-    payload, header = JWT.decode bearer, ENV['JWT_SECRET'], true, options
+    begin
+      options = { algorithm: 'HS256', iss: ENV['JWT_ISSUER'] }
+      bearer = env.fetch('HTTP_AUTHORIZATION', '').slice(7..-1)
+      payload, header = JWT.decode bearer, ENV['JWT_SECRET'], true, options
+
+      # env[:scopes] = payload['scopes']
+      # @valid_user = payload['user']
+
+    rescue JWT::DecodeError
+      [401, { 'Content-Type' => 'text/plain' }, ['A token must be passed.']]
+    rescue JWT::ExpiredSignature
+      [403, { 'Content-Type' => 'text/plain' }, ['The token has expired.']]
+    rescue JWT::InvalidIssuerError
+      [403, { 'Content-Type' => 'text/plain' }, ['The token does not have a valid issuer.']]
+    rescue JWT::InvalidIatError
+      [403, { 'Content-Type' => 'text/plain' }, ['The token does not have a valid "issued at" time.']]
+    end
+
     user = payload['user']
     valid_user = User.find_by(user_name: user['username'])
 
