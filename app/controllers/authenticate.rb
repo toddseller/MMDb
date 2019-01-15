1d069bd8e71d4e9132e05abf39cc559c806d2e00
Todd Seller <todd.seller@optimizely.com>
2019-01-15 12:20:49 -0800

    Tweak authenticate route

--- app/controllers/authenticate.rb
+++ app/controllers/authenticate.rb
@@ -1,26 +1,24 @@
-namespace '/authenticate' do
-  post '/authenticate' do
-    before do
-      content_type 'application/json'
-    end
+post '/authenticate' do
+  before do
+    content_type 'application/json'
+  end
 
-    user = User.find_by(email: params[:username_email]) || User.find_by(user_name: params[:username_email])
-    if user && user.authenticate(params[:password])
-    #   session[:user_id] = user.id
-    #   session[:name] = user.full_name
-    #   session[:theme] = user.theme
-    #   if request.xhr?
-    #     json status: "true", user_id: user.id
-    #   else
-    #     erb :'/users/show'
-    #   end
-    # else
-    #   if request.xhr?
-    #     json status: "false"
-    #   end
-      {message: "You've logged in. Yay you!!"}.to_json
-    else
-      halt 404
-    end
+  user = User.find_by(email: params[:username_email]) || User.find_by(user_name: params[:username_email])
+  if user && user.authenticate(params[:password])
+  #   session[:user_id] = user.id
+  #   session[:name] = user.full_name
+  #   session[:theme] = user.theme
+  #   if request.xhr?
+  #     json status: "true", user_id: user.id
+  #   else
+  #     erb :'/users/show'
+  #   end
+  # else
+  #   if request.xhr?
+  #     json status: "false"
+  #   end
+    {message: "You've logged in. Yay you!!"}.to_json
+  else
+    halt 404
   end
 end
