daa85abf723fa5bcb9edf2af77ba427fa9a886d1
Todd Seller <todd.seller@optimizely.com>
2019-01-15 13:59:49 -0800

    Retry authentication

--- app/controllers/api_v2_controller.rb
+++ app/controllers/api_v2_controller.rb
@@ -6,9 +6,25 @@ namespace '/api/v2' do
   post '/authenticate' do
     user = User.find_by(email: params[:username_email]) || User.find_by(user_name: params[:username_email])
     if user && user.authenticate(params[:password])
-      {message: "You've logged in. Yay you!!", user: user}.to_json
+      {message: "You've logged in. Yay you!!", token: token(user)}.to_json
     else
       halt 404
+    end
+
+    def token user
+      JWT.encode payload(user), ENV['JWT_SECRET'], 'HS256'
+    end
+
+    def payload user
+      {
+          exp: Time.now.to_i + 60 * 60,
+          iat: Time.now.to_i,
+          iss: ENV['JWT_ISSUER'],
+          user: {
+              username: user.user_name,
+              fullname: user.full_name
+          }
+      }
     end
   end
 
