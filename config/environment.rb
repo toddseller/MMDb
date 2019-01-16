00bf713f0d30e95749541d8fd177b396b5a5541a
Todd Seller <todd.seller@optimizely.com>
2019-01-15 19:25:00 -0800

    Try to resolve namespace conflict

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,15 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 require 'sinatra/json'
 require 'sinatra/base'
+self.instance_eval do
+  alias :namespace_pre_sinatra :namespace if self.respond_to?(:namespace, true)
+end
+
 require 'sinatra/namespace'
+
+self.instance_eval do
+  alias :namespace :namespace_pre_sinatra if self.respond_to?(:namespace_pre_sinatra, true)
+end
 require 'tilt/erb'
 require 'rack/cors'
 require 'jwt'
