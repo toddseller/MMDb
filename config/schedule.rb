8baf6ad4574e757f89fd42890e45f2b0c1905bc3
Todd Seller <me@toddseller.com>
2019-02-16 15:35:53 -0800

    Try to integrate cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 job_type :sidekiq, "cd :path && :environment_variable=:environment bundle exec sidekiq-client push :task :output"
 
-every 12.hours do
+every 2.minutes do
   sidekiq "TokenWorker"
 end
